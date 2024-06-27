b06ffbeb42155b8a4ee892ad54ceb4d9073ff62d
faithcherotich <faithcherotich910@gmail.com>
2024-06-27 14:29:10 +0300

    used interpolation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = 'Faith Mutai';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome, ' + currentUser.slice(0, 1) + '!';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
